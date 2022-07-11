cc4dd79c7c9aa652d8fdf1df0d07b901105450e4
SpawnMetal <spawnmetal666@gmail.com>
2022-07-11 11:49:36 +0700

    Updated

--- test.js
+++ test.js
@@ -6,12 +6,16 @@ const promise = x => Promise.resolve(x).then(x => log(x))
 
 const otherPromise = x =>
   new Promise(res => {
-    log(x)
-    res(x + 2)
+    log(x) // 2 = 4
+    res(x + 2) // 5 = 6
   }).then(log)
 
-log(1)
-timeout(2)
-promise(3)
-otherPromise(4)
-log(5)
+log(1) // 1
+timeout(2) // 6 = 2
+promise(3) // 4 = 3
+otherPromise(4) // 2 = 4 и 5 = 6
+log(5) // 3 = 5
+
+// const a = 1
+// setTimeout(() => log(a + 2)) // 3
+// Promise.resolve().then(() => log(a + 1)) // 2
