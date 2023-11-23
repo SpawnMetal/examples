fe9e6fed883bff2b318eeba4854014b36073e133
SpawnMetal <spawnmetal666@gmail.com>
2023-11-23 13:28:37 +0700

    Updated 2023.11.23

--- examples.tsx
+++ examples.tsx
@@ -119,170 +119,240 @@
  *   [{"id": "...", "text": "...", "likes":{"like": 123, "dislike": 456}}]
  */
 
-interface MyComment {
-  id: string
-  text: string
+// interface MyComment {
+//   id: string
+//   text: string
+// }
+
+// interface Page {
+//   comments: MyComment[]
+//   pageCount: number
+// }
+
+// interface Pages {
+//   [id: string]: Page
+// }
+
+// interface Like {
+//   like: number
+//   dislike: number
+// }
+
+// interface Likes {
+//   [id: string]: Like
+// }
+
+// type Result = Array<{
+//   id: string
+//   text: string
+//   likes: Likes
+// }>
+
+// async function getComments(pageNumber: number): Promise<Page> {
+//   const pageCount = 5
+
+//   const pages: Pages = {
+//     '1': {
+//       pageCount,
+//       comments: [
+//         {
+//           id: '1',
+//           text: 'page 1, comment 1',
+//         },
+//         {
+//           id: '2',
+//           text: 'page 1, comment 2',
+//         },
+//       ],
+//     },
+//     '2': {
+//       pageCount,
+//       comments: [
+//         {
+//           id: '3',
+//           text: 'page 2, comment 3',
+//         },
+//       ],
+//     },
+//     '3': {
+//       pageCount,
+//       comments: [
+//         {
+//           id: '4',
+//           text: 'page 3, comment 4',
+//         },
+//       ],
+//     },
+//     '4': {
+//       pageCount,
+//       comments: [
+//         {
+//           id: '5',
+//           text: 'page 4, comment 5',
+//         },
+//         {
+//           id: '6',
+//           text: 'page 4, comment 6',
+//         },
+//         {
+//           id: '7',
+//           text: 'page 4, comment 7',
+//         },
+//       ],
+//     },
+//     '5': {
+//       pageCount,
+//       comments: [
+//         {
+//           id: '8',
+//           text: 'page 5, comment 8',
+//         },
+//       ],
+//     },
+//   }
+
+//   return new Promise((resolve, reject) => {
+//     resolve(pages[pageNumber])
+//   })
+// }
+
+// async function getLikes(commentId: string): Promise<Like> {
+//   const likes: Likes = {
+//     '1': {like: 10, dislike: 1},
+//     '2': {like: 20, dislike: 2},
+//     '3': {like: 30, dislike: 3},
+//     '4': {like: 40, dislike: 4},
+//     '5': {like: 50, dislike: 5},
+//     '6': {like: 60, dislike: 6},
+//     '7': {like: 70, dislike: 7},
+//     '8': {like: 80, dislike: 8},
+//   }
+
+//   return new Promise((resolve, reject) => {
+//     resolve(likes[commentId])
+//   })
+// }
+
+// const result: Result[] = []
+
+// async function getData(): Promise<Result[]> {
+//   const commentsPromise: Promise<Page>[] = []
+//   const comment1 = await getComments(1)
+//   const pageCount = comment1.pageCount
+
+//   const fillResult = comment => getLikes(comment.id).then(like => result.push({...comment, likes: like}))
+
+//   for (let page = 2; page <= pageCount; page++) commentsPromise.push(getComments(page))
+//   for (const comment of comment1.comments) fillResult(comment)
+
+//   Promise.allSettled(commentsPromise).then(commentsPromiseSettled => {
+//     //   {
+//     //     status: 'fulfilled',
+//     //     value: {
+//     //       '1': {
+//     //         pageCount,
+//     //         comments: [
+//     //           {
+//     //             id: '1',
+//     //             text: 'page 1, comment 1',
+//     //           },
+//     //           {
+//     //             id: '2',
+//     //             text: 'page 1, comment 2',
+//     //           },
+//     //         ],
+//     //       },
+//     //     },
+//     //   },
+//     //   {
+//     //     status: 'fulfilled',
+//     //     value: {
+//     //       '2': {
+//     //         pageCount,
+//     //         comments: [
+//     //           {
+//     //             id: '3',
+//     //             text: 'page 2, comment 3',
+//     //           },
+//     //         ],
+//     //       },
+//     //     },
+//     //   },
+//     // ]
+
+//     for (const commentPromiseSettled of commentsPromiseSettled) for (const comment of commentPromiseSettled.value.comments) fillResult(comment)
+//   })
+// }
+
+// getData()
+// setTimeout(() => console.log(result))
+
+// ===================================================== //
+// Жизненный цикл
+
+export const Child = ({num}) => {
+  console.log('child: render')
+
+  useLayoutEffect(() => {
+    console.log('child: layout effect')
+
+    return () => {
+      console.log('child: cleanup layout effect')
+    }
+  }, [num])
+
+  useEffect(() => {
+    console.log('child: effect')
+    return () => {
+      console.log('child: cleanup effect')
+    }
+  }, [num])
+
+  return null
 }
 
-interface Page {
-  comments: MyComment[]
-  pageCount: number
-}
+export const App = () => {
+  const [num, setNum] = useState(0)
 
-interface Pages {
-  [id: string]: Page
-}
+  console.log('parent: render')
 
-interface Like {
-  like: number
-  dislike: number
-}
-
-interface Likes {
-  [id: string]: Like
-}
-
-type Result = Array<{
-  id: string
-  text: string
-  likes: Likes
-}>
-
-async function getComments(pageNumber: number): Promise<Page> {
-  const pageCount = 5
-
-  const pages: Pages = {
-    '1': {
-      pageCount,
-      comments: [
-        {
-          id: '1',
-          text: 'page 1, comment 1',
-        },
-        {
-          id: '2',
-          text: 'page 1, comment 2',
-        },
-      ],
-    },
-    '2': {
-      pageCount,
-      comments: [
-        {
-          id: '3',
-          text: 'page 2, comment 3',
-        },
-      ],
-    },
-    '3': {
-      pageCount,
-      comments: [
-        {
-          id: '4',
-          text: 'page 3, comment 4',
-        },
-      ],
-    },
-    '4': {
-      pageCount,
-      comments: [
-        {
-          id: '5',
-          text: 'page 4, comment 5',
-        },
-        {
-          id: '6',
-          text: 'page 4, comment 6',
-        },
-        {
-          id: '7',
-          text: 'page 4, comment 7',
-        },
-      ],
-    },
-    '5': {
-      pageCount,
-      comments: [
-        {
-          id: '8',
-          text: 'page 5, comment 8',
-        },
-      ],
-    },
+  function clickHandler() {
+    setNum(prev => prev + 1)
   }
 
-  return new Promise((resolve, reject) => {
-    resolve(pages[pageNumber])
-  })
+  useLayoutEffect(() => {
+    console.log('parent: layout effect')
+
+    return () => {
+      console.log('parent: cleanup layout effect')
+    }
+  }, [num])
+
+  useEffect(() => {
+    console.log('parent: effect')
+    return () => {
+      console.log('parent: cleanup effect')
+    }
+  }, [num])
+
+  return (
+    <>
+      <Child num={num} />
+      <button onClick={clickHandler}>render</button>
+      <div style={{fontSize: '45px', textAlign: 'center'}}>{num}</div>
+    </>
+  )
 }
 
-async function getLikes(commentId: string): Promise<Like> {
-  const likes: Likes = {
-    '1': {like: 10, dislike: 1},
-    '2': {like: 20, dislike: 2},
-    '3': {like: 30, dislike: 3},
-    '4': {like: 40, dislike: 4},
-    '5': {like: 50, dislike: 5},
-    '6': {like: 60, dislike: 6},
-    '7': {like: 70, dislike: 7},
-    '8': {like: 80, dislike: 8},
-  }
-
-  return new Promise((resolve, reject) => {
-    resolve(likes[commentId])
-  })
-}
-
-const result: Result[] = []
-
-async function getData(): Promise<Result[]> {
-  const commentsPromise: Promise<Page>[] = []
-  const comment1 = await getComments(1)
-  const pageCount = comment1.pageCount
-
-  const fillResult = comment => getLikes(comment.id).then(like => result.push({...comment, likes: like}))
-
-  for (let page = 2; page <= pageCount; page++) commentsPromise.push(getComments(page))
-  for (const comment of comment1.comments) fillResult(comment)
-
-  Promise.allSettled(commentsPromise).then(commentsPromiseSettled => {
-    //   {
-    //     status: 'fulfilled',
-    //     value: {
-    //       '1': {
-    //         pageCount,
-    //         comments: [
-    //           {
-    //             id: '1',
-    //             text: 'page 1, comment 1',
-    //           },
-    //           {
-    //             id: '2',
-    //             text: 'page 1, comment 2',
-    //           },
-    //         ],
-    //       },
-    //     },
-    //   },
-    //   {
-    //     status: 'fulfilled',
-    //     value: {
-    //       '2': {
-    //         pageCount,
-    //         comments: [
-    //           {
-    //             id: '3',
-    //             text: 'page 2, comment 3',
-    //           },
-    //         ],
-    //       },
-    //     },
-    //   },
-    // ]
-
-    for (const commentPromiseSettled of commentsPromiseSettled) for (const comment of commentPromiseSettled.value.comments) fillResult(comment)
-  })
-}
-
-getData()
-setTimeout(() => console.log(result))
+// parent: render
+// develop // parent: render
+// child: render
+// develop // child: render
+// child: cleanup layout effect
+// parent: cleanup layout effect
+// child: layout effect
+// parent: layout effect
+// child: cleanup effect
+// parent: cleanup effect
+// child: effect
+// parent: effect
